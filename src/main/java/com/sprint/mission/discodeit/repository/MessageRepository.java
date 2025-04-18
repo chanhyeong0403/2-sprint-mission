cdbf950e4d1308b5a4395d179bedaa1b5fbf7185
chanhyeong0403 <parkchanhyeong0403@gmail.com>
2025-04-18 15:12:39 +0900

    refactor: 기존의 Repository 인터페이스를 JpaRepository로 정의

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -2,21 +2,20 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Message;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MessageRepository {
-
-  Message save(Message message);
-
-  Optional<Message> findById(UUID id);
+public interface MessageRepository extends JpaRepository<Message, UUID> {
 
   List<Message> findAllByChannelId(UUID channelId);
 
-  boolean existsById(UUID id);
-
-  void deleteById(UUID id);
+  @Query("SELECT MAX(m.createdAt) FROM Message m WHERE m.channel.id = :channelId")
+  Optional<Instant> findLastMessageAtByChannelId(@Param("channelId") UUID channelId);
 
   void deleteAllByChannelId(UUID channelId);
 }
